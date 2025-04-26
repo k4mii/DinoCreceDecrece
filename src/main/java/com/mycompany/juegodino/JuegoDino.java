4ac6d1aff169d52b6b27f3d0056fe67092ef29be
Camila <mariac.pradac@autonoma.edu.co>
2025-04-25 21:36:31 -0500

    Se organiza el .gitignore

---
+++ src/main/java/com/mycompany/juegodino/JuegoDino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juegodino;
+
+/**
+ *
+ * @author Kamii
+ */
+public class JuegoDino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
